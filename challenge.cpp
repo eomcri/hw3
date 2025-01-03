72f92d50917775237444b082eb9517227ad9a3d7
eomcri <jihun.eomcri@gmail.com>
2025-01-03 13:28:06 +0900

    hw3 main and challenge
    make SimpleVector class with functions and copy constructor

--- challenge.cpp
+++ challenge.cpp
@@ -21,6 +21,15 @@ SimpleVector(int capacity){
 ~SimpleVector(){
     delete[] data;
 }
+// copy constructor
+SimpleVector(const SimpleVector& obj){
+    data = new T[obj.currentCapacity];
+    for (int i = 0; i < obj.currentSize; i++)
+        data[i] = obj.data[i];
+    currentSize = obj.currentSize;
+    currentCapacity = obj.currentCapacity;
+}
+
 void push_back(const T& value){
     if(currentSize < currentCapacity){
         data[currentSize] = value;
@@ -44,15 +53,16 @@ int capacity(){
 
 int main(){
     SimpleVector<int> vector = SimpleVector<int>();
-    cout << "Current capacity: " << vector.capacity() << endl;
-    cout << "Current size: " << vector.size() << endl;
-    vector.push_back(1);
-    cout << "Push back. " << "Current size: " << vector.size() << endl;
-    vector.pop_back();
     cout << "Pop back. " << "Current size: "  << vector.size() << endl;
     for(int i = 0; i < 10; i++)
         vector.push_back(i);
     cout << "Push back 10 elements. " << "Current size: "  << vector.size() << endl;
-    vector.push_back(1);
-    cout << "Vector is full. " << "Current size: "  << vector.size() << endl;
+    
+    // copy vector
+    SimpleVector<int> vectorCopy = SimpleVector<int>(vector);
+    cout << "Current size: " << vectorCopy.size() << endl;
+    vectorCopy.pop_back();
+    cout << "vectorCopy current size: " << vectorCopy.size() << endl;
+    cout << "vector current size: " << vector.size() << endl;
+
 }
